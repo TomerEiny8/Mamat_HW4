ae713560790bd328b702da711742ae21b452f006
TomerEiny8 <tomerkorial@gmail.com>
2025-05-27 13:27:29 +0300

    grades.c

---
+++ grades.c
@@ -0,0 +1,21 @@
+#include "grades.h"
+#include <stdlib.h>
+#include <string.>
+#include "linked-list.h"
+
+typedef struct course {
+	char* name;
+	int grade;
+} *Course;
+
+typedef struct student {
+	char* name;
+	int id;
+	struct list *courses;
+} *Student;
+
+struct grades {
+	struct list *students;
+};
+
+
